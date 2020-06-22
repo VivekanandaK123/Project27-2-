a5d64c1a681759496e64cf164a914039ac917098
VivekanandaK123 <vivekanandakatakam16@gmail.com>
2020-06-22 16:27:17 +0530

    Pro

---
+++ sketch.js
@@ -0,0 +1,60 @@
+var engine ,world;
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Constraint = Matter.Constraint;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+	engine = Engine.create();
+	world = engine.world;
+	Engine.run(engine);
+
+	BobObject1 = new Bob(270,450,25);
+	BobObject2 = new Bob(320,450,25);
+	BobObject3 = new Bob(370,450,25);
+    BobObject4 = new Bob(420,450,25);
+	BobObject5 = new Bob(470,450,25);
+	
+	roofObject = new Roof(370,100,300,25);
+
+	rope1 = new Rope(BobObject1.body, roofObject.body, 275, 0);
+	rope2 = new Rope(BobObject2.body, roofObject.body, 320, 0);
+	rope3 = new Rope(BobObject3.body, roofObject.body, 370, 0);
+	rope4 = new Rope(BobObject4.body, roofObject.body, 420, 0);
+	rope5 = new Rope(BobObject5.body, roofObject.body, 470, 0);
+
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(235);
+  
+  drawSprites();
+ 
+ BobObject1.display();
+ BobObject2.display();
+ BobObject3.display();
+ BobObject4.display();
+ BobObject5.display();
+
+ roofObject.display();
+
+ rope1.display();
+ rope2.display();
+ rope3.display();
+ rope4.display();
+ rope5.display();
+ 
+
+}
+
+
+
